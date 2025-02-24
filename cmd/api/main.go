517f1b7302b4aaaba86e79df34681b93a9c14ba7
sulaiman-macrometa <olabisisulaiman@gmail.com>
2025-02-24 10:13:20 +0100

    chore: set up server and logger middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gorilla/mux"
+	"github.com/horlabyc/grocery-app/internal/middleware"
 	"github.com/horlabyc/grocery-app/internal/storage/postgres"
 	"github.com/horlabyc/grocery-app/internal/utils"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -31,4 +40,52 @@ func main() {
 
 	defer db.Close()
 	fmt.Println("Connected to database")
+
+	// Initialize Router
+	router := mux.NewRouter().PathPrefix("/api").Subrouter()
+
+	//Apply Middleware
+	router.Use(middleware.LoggerMiddleware)
+
+	//Apply CORS Middleware
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	serverPort := utils.GetEnv("SERVER_PORT", "8080")
+
+	// Create Server
+	srv := &http.Server{
+		Addr:         "localhost:" + serverPort,
+		Handler:      c.Handler(router),
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	// Start Server in a goroutine
+	log.Println("Starting server on port", serverPort)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit // blocks until signal is received
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
+
+	log.Println("Server exited properly")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/horlabyc/grocery-app
 go 1.23.4
 
 require (
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 )
